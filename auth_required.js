88f44247f69c9644f989a7597e82a9640e911bbe
Terabyte17 <yash.sahijwani.ece19@itbhu.ac.in>
2020-08-30 19:10:31 +0530

    correct link added

--- auth_required.js
+++ auth_required.js
@@ -1,7 +1,7 @@
 var auth_token = localStorage.getItem('token');
 if(auth_token===null)
 {
-    window.location.href='login/';
+    window.location.href='login/index.html';
     console.log("I have not been authorized");
 }
 /***
