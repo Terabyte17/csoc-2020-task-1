6ec2ac91bf2ecd0228078c7cd4e348e4fd303322
Terabyte17 <yash.sahijwani.ece19@itbhu.ac.in>
2020-08-30 19:11:55 +0530

    authentication added

--- auth_required.js
+++ auth_required.js
@@ -2,7 +2,6 @@ var auth_token = localStorage.getItem('token');
 if(auth_token===null)
 {
     window.location.href='login/index.html';
-    console.log("I have not been authorized");
 }
 /***
  * @todo Redirect the user to login page if token is not present.
