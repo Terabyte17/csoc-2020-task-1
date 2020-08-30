0dddcc45d24f81e8a36205cf590a9aa719e38d3f
Terabyte17 <yash.sahijwani.ece19@itbhu.ac.in>
2020-08-30 19:35:25 +0530

    link corrected

--- auth_required.js
+++ auth_required.js
@@ -1,7 +1,7 @@
 var auth_token = localStorage.getItem('token');
 if(auth_token === undefined)
 {
-    window.location.href='/login/index.html';
+    window.location.href='/login';
 }
 /***
  * @todo Redirect the user to login page if token is not present.
