592e2f7b9820222ca50aa8432d43219b8799121e
Terabyte17 <yash.sahijwani.ece19@itbhu.ac.in>
2020-08-30 19:08:45 +0530

    authorization

--- auth_required.js
+++ auth_required.js
@@ -1,7 +1,7 @@
 var auth_token = localStorage.getItem('token');
 if(auth_token===null)
 {
-    window.location.href='auth/login/';
+    window.location.href='login/';
     console.log("I have not been authorized");
 }
 /***
