f5fcd527f167c2333a3272921a6032241d4e7c8f
Terabyte17 <yash.sahijwani.ece19@itbhu.ac.in>
2020-08-30 19:06:52 +0530

    script removed

--- auth_required.js
+++ auth_required.js
@@ -1,10 +1,8 @@
-function not_authorized(){
 var auth_token = localStorage.getItem('token');
 if(auth_token===null)
 {
     window.location.href='auth/login/';
     console.log("I have not been authorized");
-}
 }
 /***
  * @todo Redirect the user to login page if token is not present.
