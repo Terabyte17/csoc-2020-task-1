20077c3338f03eab2cd0ba6477087bc7073c5686
Terabyte17 <yash.sahijwani.ece19@itbhu.ac.in>
2020-08-30 18:59:11 +0530

    authorization

--- auth_required.js
+++ auth_required.js
@@ -1,3 +1,9 @@
+var auth_token = localStorage.getItem('token');
+if(auth_token===null)
+{
+    window.location.href='auth/login/';
+    console.log("I have not been authorized");
+}
 /***
  * @todo Redirect the user to login page if token is not present.
  */
--- no_auth_required.js
+++ no_auth_required.js
@@ -1,3 +1,10 @@
+var auth_token = localStorage.getItem('token');
+if(auth_token!==null)
+{
+    window.location.href='/';
+    console.log("I have been authorized");
+}
+
 /***
  * @todo Redirect the user to main page if token is present.
  */
